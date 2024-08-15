09ee8cd202d9e64f92669ddecdef7492a565033d
GonzaloRuarte <gonzalorpg@gmail.com>
2024-08-15 13:46:12 -0300

    TODO split into trials test

--- pyxations/pyxations/__init__.py
+++ pyxations/pyxations/__init__.py
@@ -2,7 +2,7 @@
 
 from .bids_formatting import dataset_to_bids, compute_derivatives_for_dataset
 from .eye_movement_detection import RemodnavDetection
-from .post_processing import saccades_direction
+from .post_processing import saccades_direction,split_into_trials, get_timestamps_from_messages
 from .visualization import Visualization
 
-__all__ = ["dataset_to_bids", "compute_derivatives_for_dataset", "RemodnavDetection","saccades_direction", "Visualization"]
+__all__ = ["dataset_to_bids", "compute_derivatives_for_dataset", "RemodnavDetection","saccades_direction", "Visualization", "split_into_trials", "get_timestamps_from_messages"]
---
+++ pyxations/tests/test_split_into_trials.py
@@ -0,0 +1,13 @@
+import unittest
+from pyxations import split_into_trials
+
+class TestSplitIntoTrials(unittest.TestCase):
+    def test_split_into_trials(self):
+
+        # TODO
+        
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
