849e588c7d058cedcfe28fa316f6dcec6f5e010a
GonzaloRuarte <gonzalorpg@gmail.com>
2024-08-15 11:55:20 -0300

    test scanpath

--- pyxations/pyxations/visualization.py
+++ pyxations/pyxations/visualization.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import matplotlib.colors as mplcolors
 import numpy as np
 import pandas as pd
 from . import saccades_direction
@@ -8,21 +10,155 @@ class Visualization():
     def __init__(self, session_folder_path):
         self.session_folder_path = session_folder_path
 
-    def load_fixations(self):
-        path_to_fixations = path.join(self.session_folder_path, 'fix.hdf5')
-        fixations = pd.read_hdf(path_or_buf=path_to_fixations)
-        return fixations
 
-    def load_saccades(self):
-        path_to_saccades = path.join(self.session_folder_path, 'sacc.hdf5')
-        saccades = pd.read_hdf(path_or_buf=path_to_saccades)
-        return saccades
+    def load_derivative(self, file_name:str):
+        path_to_file = path.join(self.session_folder_path, file_name)
+        if path.exists(path_to_file):
+            file = pd.read_hdf(path_or_buf=path_to_file)
+        else:
+            raise FileNotFoundError(f'File {path_to_file} not found')
+        return file
+
+    def scanpath(self,tmin:float, tmax:float, img_path:str=None, 
+             screen_res_x:int=1920, screen_res_y:int=1080):
+        """
+        Plots the scanpath, including fixations, saccades, and optionally an image background and gaze samples.
+
+        Parameters
+        ----------
+        tmin : float
+            The minimum time for filtering the data.
+        tmax : float
+            The maximum time for filtering the data.
+        img_path : str, optional
+            Path to an image file to be used as a background (default is None).
+        screen_res_x : int, optional
+            Horizontal resolution of the screen in pixels (default is 1920).
+        screen_res_y : int, optional
+            Vertical resolution of the screen in pixels (default is 1080).
+
+        Returns
+        -------
+        fig : matplotlib.figure.Figure
+            The figure object containing the plot.
+        fixations : pd.DataFrame
+            The filtered fixations DataFrame.
+        saccades : pd.DataFrame
+            The filtered saccades DataFrame.
+        samples : pd.DataFrame
+            The filtered samples DataFrame.
+        """
+        fixations = self.load_derivative('fix.hdf5')
+        try:
+            samples = self.load_derivative('samples.hdf5')
+        except:
+            samples = None
+        try:
+            saccades = self.load_derivative('sacc.hdf5')
+        except:
+            saccades = None
+
+
+        #----- Filter saccades, fixations and samples to defined time interval -----#
+        fixations = fixations.loc[(fixations['tStart'] >= tmin) & (fixations['tStart'] <= tmax)].reset_index(drop=True)
+        if type(saccades) == pd.DataFrame:
+            saccades = saccades.loc[(saccades['tStart'] >= tmin) & (saccades['tStart'] <= tmax)]
+        if type(samples) == pd.DataFrame:
+            samples = samples.loc[(samples['tSample'] >= tmin) & (samples['tSample'] <= tmax)]
+
+
+        #----- Define figure and axes -----#
+        if type(samples) == pd.DataFrame:
+            fig, axs = plt.subplots(nrows=2, ncols=1, height_ratios=(4, 1),figsize=(10, 6))
+            ax_main = axs[0]
+            ax_gaze = axs[1]
+        else:
+            fig, ax_main = plt.subplots(figsize=(10, 6))
+        
+        fig.suptitle(f'Scanpath tmin:{tmin} - tmax:{tmax}')
+        ax_main.set_xlim(0, screen_res_x)
+        ax_main.set_ylim(0, screen_res_y)
+
+
+        #----- Plot fixations as dots if any in time interval -----#
+        # Colormap: Get fixation durations for scatter circle size
+        sizes = fixations['duration']
+        
+        # Define rainwbow cmap for fixations
+        cmap = plt.cm.rainbow
+        
+        # Define the bins and normalize
+        fix_num = fixations.index + 1
+        bounds = np.linspace(1, fix_num[-1] + 1, fix_num[-1] + 1)
+        norm = mplcolors.BoundaryNorm(bounds, cmap.N)
+        
+        # Plot
+        ax_main.scatter(fixations['xAvg'], fixations['yAvg'], c=fix_num, s=sizes, cmap=cmap, norm=norm, alpha=0.5, zorder=2)
+
+        # Colorbar
+        PCM = ax_main.get_children()[0]  # When the fixations dots for color mappable were ploted (first)
+        cb = plt.colorbar(PCM, ax=ax_main, ticks=[fix_num[0] + 1/2, fix_num[int(len(fix_num)/2)]+1/2, fix_num[-1]+1/2], fraction=0.046, pad=0.04)
+        cb.ax.set_yticklabels([fix_num[0], fix_num[int(len(fix_num)/2)], fix_num[-1]])
+        cb.set_label('# of fixation')
+
+
+        #----- Plot image if provided -----#
+        if img_path != None:
+            # Load search image
+            img = mpimg.imread(img_path)
+            
+            # Get image size
+            img_res_x = img.shape[1]
+            img_res_y = img.shape[0]
+            
+            # Define box in axes to plot image
+            image_box_extent = ((screen_res_x - img_res_x) / 2, img_res_x + (screen_res_x - img_res_x) / 2, (screen_res_y - img_res_y) / 2, img_res_y + (screen_res_y - img_res_y) / 2) 
+            
+            # Plot
+            ax_main.imshow(img, extent=image_box_extent, zorder=0)
+
+        #----- Plot scanpath and gaze if samples provided -----#
+        if type(samples) == pd.DataFrame:
+
+            # Scanpath
+            ax_main.plot(samples['LX'], samples['LY'], '--', color='C0', zorder=1)
+            ax_main.plot(samples['RX'], samples['RY'], '--', color='black', zorder=1)
+            
+            # Gaze
+            ax_gaze.plot(samples['tSample'], samples['LX'], label='Left X')
+            ax_gaze.plot(samples['tSample'], samples['LY'], label='Left Y')
+            ax_gaze.plot(samples['tSample'], samples['RX'], label='Right X')
+            ax_gaze.plot(samples['tSample'], samples['RY'], label='Right Y')
+
+            plot_min, plot_max = ax_gaze.get_ylim()
+
+            # Plot fixations as color span in gaze axes
+            for fix_idx, fixation in fixations.iterrows():
+                color = cmap(norm(fix_idx + 1))
+                ax_gaze.axvspan(ymin=0, ymax=1, xmin=fixation['tStart'], xmax=fixation['tStart'] + fixation['duration'], color=color, alpha=0.4, label='fix')
+            
+            # Plor saccades as vlines in gaze axes
+            if type(saccades) == pd.DataFrame:
+                for _, saccade in saccades.iterrows():
+                    ax_gaze.vlines(x=saccade['tStart'], ymin=plot_min, ymax=plot_max, colors='red', linestyles='--', label='sac')
+
+            # Legend
+            handles, labels = plt.gca().get_legend_handles_labels()
+            by_label = dict(zip(labels, handles))
+            plt.legend(by_label.values(), by_label.keys(),  loc='center left', bbox_to_anchor=(1, 0.5))
+            ax_gaze.set_ylabel('Gaze')
+            ax_gaze.set_xlabel('Time [s]')
+        # Save figure as "scanpath.png"
+        plt.tight_layout()
+        fig.savefig(path.join(self.session_folder_path, 'scanpath.png'))
+
+        
 
     @staticmethod
     def duration(axs=None):
         def decorator(func):
-            def wrapper():
-                fixations = func()
+            def wrapper(*args, **kwargs):
+                fixations = func(*args, **kwargs)
                 ax = axs
                 print('Plotting fixation duration histogram')
 
@@ -41,8 +177,8 @@ class Visualization():
     @staticmethod
     def amplitude(axs=None):
         def decorator(func):
-            def wrapper():
-                saccades = func()
+            def wrapper(*args, **kwargs):
+                saccades = func(*args, **kwargs)
 
                 print('Plotting saccades amplitude histogram')
                 ax = axs
@@ -62,8 +198,8 @@ class Visualization():
     @staticmethod
     def direction(axs=None,figs=None):
         def decorator(func):
-            def wrapper():
-                saccades = func()
+            def wrapper(*args, **kwargs):
+                saccades = func(*args, **kwargs)
 
                 print('Plotting saccades direction histogram')
                 ax = axs
@@ -99,8 +235,8 @@ class Visualization():
     @staticmethod
     def main_sequence(axs=None, hline=None):
         def decorator(func):
-            def wrapper():
-                saccades = func()
+            def wrapper(*args, **kwargs):
+                saccades = func(*args, **kwargs)
 
                 print('Plotting main sequence')
                 ax = axs
@@ -131,19 +267,18 @@ class Visualization():
         fig, axs = plt.subplots(2, 2, figsize=(12, 7))
         
         # Load fixations and apply the duration decorator
-        decorated_load_fixations = self.duration(axs=axs[0, 0])(self.load_fixations)
-        decorated_load_fixations()
+        decorated_load_fixations = self.duration(axs=axs[0, 0])(self.load_derivative)
+        decorated_load_fixations("fix.hdf5")
 
         # Apply main sequence decorator
-        main_sequence = self.main_sequence(axs=axs[1, 1])(self.load_saccades)
-        print(self.load_saccades())
+        main_sequence = self.main_sequence(axs=axs[1, 1])(self.load_derivative)
 
         # Apply direction decorator
         direction = self.direction(axs=axs[1, 0],figs=fig)(main_sequence)
 
         # Apply amplitude decorator
         amplitude = self.amplitude(axs=axs[0, 1])(direction)
-        amplitude()
+        amplitude("sacc.hdf5")
 
         fig.tight_layout()
         plt.savefig(path.join(self.session_folder_path, 'multipanel.png'))
---
+++ pyxations/tests/test_scanpath.py
@@ -0,0 +1,32 @@
+import os
+import unittest
+from pyxations import Visualization
+import pandas as pd
+
+class TestVisualization(unittest.TestCase):
+    def test_visualization(self):
+
+        # Get path to samples from parsed edf
+        current_folder = os.getcwd()
+        current_folder = os.path.dirname(current_folder)
+
+        path_to_session = os.path.join(current_folder, "example_dataset_derivatives", "sub-ab01", "ses-second_half")
+
+        visualization = Visualization(path_to_session)
+
+        # Load samples.hdf5 file
+        path_to_samples = os.path.join(path_to_session, "samples.hdf5")
+
+        samples = pd.read_hdf(path_or_buf=path_to_samples)
+
+        img_path = os.path.join('..','example_images','test_img.jpg')
+        
+        # Plot Scanpath
+        visualization.scanpath(tmin=samples['tSample'][100000], tmax=samples['tSample'][110000], img_path=img_path)
+
+        # Assert that the file multipanel.png was created
+        self.assertTrue(os.path.exists(os.path.join(path_to_session, "scanpath.png")))
+
+
+if __name__ == "__main__":
+    unittest.main()
